2cc02ae55d50d8818529ee541467a31e18edcb1f
Gourob Mazumder <gourob.mazumder@enosisbd.com>
2025-08-15 00:35:58 +0600

    Initial Commit

---
+++ Chatly/ChatlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatlyApp.swift
+//  Chatly
+//
+//  Created by Gourob Mazumder on 15/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
